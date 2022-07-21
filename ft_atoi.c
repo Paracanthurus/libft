3e05d58ecb51bb50c305bad616a009a22665e277
Paracanthurus <okubo@MacBook-Pro.local>
2022-07-21 21:18:34 +0900

    ft_atoiのオーバーフロー処理を追加

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,19 +6,20 @@
 /*   By: aokubo <aokubo@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 22:14:47 by aokubo            #+#    #+#             */
-/*   Updated: 2022/03/01 15:19:58 by aokubo           ###   ########.fr       */
+/*   Updated: 2022/07/21 21:12:05 by aokubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
 
 static int	ft_isspace(char c);
+static long	ft_calculate(const char *str, int sign);
 
 int	ft_atoi(const char *str)
 {
-	int	i;
-	int	sign;
-	int	res;
+	size_t	i;
+	int		sign;
+	long	res;
 
 	sign = 1;
 	i = 0;
@@ -30,13 +31,9 @@ int	ft_atoi(const char *str)
 			sign = -1;
 		i++;
 	}
-	res = 0;
-	while (ft_isdigit(str[i]))
-	{
-		res *= 10;
-		res += str[i] - '0';
-		i++;
-	}
+	res = ft_calculate(&str[i], sign);
+	if (errno == ERANGE)
+		return (res);
 	return (sign * res);
 }
 
@@ -47,4 +44,30 @@ static int	ft_isspace(char c)
 	if (c == '\f' || c == '\r' || c == ' ')
 		return (1);
 	return (0);
+}
+
+static long	ft_calculate(const char *str, int sign)
+{
+	size_t				i;
+	unsigned long long	res;
+
+	i = 0;
+	res = 0;
+	while (ft_isdigit(str[i]))
+	{
+		res *= 10;
+		res += str[i] - '0';
+		i++;
+	}
+	if (sign == 1 && (i > 19 || res > (unsigned long long)LONG_MAX))
+	{
+		errno = ERANGE;
+		return (LONG_MAX);
+	}
+	if (sign == -1 && (i > 19 || res > (unsigned long long)LONG_MAX + 1))
+	{
+		errno = ERANGE;
+		return (LONG_MIN);
+	}
+	return (res);
 }
